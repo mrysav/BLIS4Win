fea8c2a1c38d096f0c0bd8c8db2175c2c7259f36
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2024-02-25 14:01:19 -0500

    Switch to GTK4 with gtk-rs

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
-#![windows_subsystem = "windows"]
 /*!
     A very simple application that shows your name in a message box.
     Unlike `basic_d`, this example uses layout to position the controls in the window
 */
 
-extern crate native_windows_derive as nwd;
-extern crate native_windows_gui as nwg;
-
-use nwg::NativeUi;
-
+use gtk::prelude::*;
+use gtk::{glib, Application, ApplicationWindow};
 use handlebars::Handlebars;
+use std::{cell::RefCell, borrow::Borrow, thread::sleep, time::Duration};
+use std::ops::Deref;
+use std::rc::Rc;
 
 pub mod process;
 use process::{
@@ -17,12 +16,10 @@ use process::{
     php::{self},
 };
 
+const APP_ID: &'static str = "org.gtk_rs.HelloWorld2";
 const BLIS_VERSION: &'static str = "4.0";
 
 pub struct BlisApp {
-    window: nwg::Window,
-    layout: nwg::GridLayout,
-
     php: php::PhpFpm,
     caddy: caddy::Caddy,
 }
@@ -38,8 +35,6 @@ impl BlisApp {
         handlebars.set_strict_mode(true);
 
         BlisApp {
-            window: Default::default(),
-            layout: Default::default(),
             php: php::PhpFpm::new(handlebars),
             caddy: caddy::Caddy::new(),
         }
@@ -54,94 +49,34 @@ impl BlisApp {
     pub fn stop(&self) {
         self.caddy.stop();
         self.php.stop();
-
-        nwg::stop_thread_dispatch();
     }
 }
 
-use std::{cell::RefCell, borrow::Borrow, thread::sleep, time::Duration};
-use std::ops::Deref;
-use std::rc::Rc;
+fn main() -> glib::ExitCode {
+    // Create a new application
+    let app = Application::builder().application_id(APP_ID).build();
+    let mut appsvc = BlisApp::new();
 
-pub struct BlisAppUi {
-    inner: Rc<BlisApp>,
-    default_handler: RefCell<Vec<nwg::EventHandler>>,
+    // Connect to "activate" signal of `app`
+    app.connect_activate(build_ui);
+
+    appsvc.start();
+
+    // Run the application
+    app.run()
 }
 
-impl nwg::NativeUi<BlisAppUi> for BlisApp {
-    fn build_ui(mut data: BlisApp) -> Result<BlisAppUi, nwg::NwgError> {
-        use nwg::Event as E;
+fn build_ui(app: &Application) {
+    // Create a window and set the title
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("My GTK App")
+        .build();
 
-        // Controls
-        nwg::Window::builder()
-            .size((300, 150))
-            .position((300, 300))
-            .title(format!("BLIS v{}", BLIS_VERSION).as_str())
-            .build(&mut data.window)?;
-
-        // Wrap-up
-        let ui = BlisAppUi {
-            inner: Rc::new(data),
-            default_handler: Default::default(),
-        };
-
-        // Events
-        let window_handles = [&ui.window.handle];
-        for handle in window_handles.iter() {
-            let evt_ui = Rc::downgrade(&ui.inner);
-            let handle_events = move |evt, _evt_data, handle| {
-                if let Some(evt_ui) = evt_ui.upgrade() {
-                    match evt {
-                        E::OnWindowClose => {
-                            if &handle == &evt_ui.window {
-                                BlisApp::stop(&evt_ui);
-                            }
-                        }
-                        _ => {}
-                    }
-                }
-            };
-
-            ui.default_handler
-                .borrow_mut()
-                .push(nwg::full_bind_event_handler(handle, handle_events));
-        }
-
-        // Layouts
-        nwg::GridLayout::builder()
-            .parent(&ui.window)
-            .spacing(2)
-            .min_size([150, 140])
-            .build(&ui.layout)?;
-
-        return Ok(ui);
-    }
+    // Present window
+    window.present();
 }
 
-impl Drop for BlisAppUi {
-    /// To make sure that everything is freed without issues, the default handler must be unbound.
-    fn drop(&mut self) {
-        let mut handlers = self.default_handler.borrow_mut();
-        for handler in handlers.drain(0..) {
-            nwg::unbind_event_handler(&handler);
-        }
-    }
-}
-
-impl Deref for BlisAppUi {
-    type Target = BlisApp;
-
-    fn deref(&self) -> &BlisApp {
-        return &self.inner;
-    }
-}
-
-fn main() {
-    nwg::init().expect("Failed to init Native Windows GUI");
-    nwg::Font::set_global_family("Segoe UI").expect("Failed to set default font");
-
-    let mut app = BlisApp::new();
-    app.start();
-    let _app = BlisApp::build_ui(app).expect("Failed to build UI");
-    nwg::dispatch_thread_events();
+fn teardown(app: &Application) {
+    // TODO
 }
